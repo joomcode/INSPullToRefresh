dcd5abd2da900e7c59e2ebcf08c27d43e82f52f7
Makarov Yury <makarov.yuriy@gmail.com>
2017-10-14 13:36:10 +0300

    Remove all navigation controller related hacking, since it may break app's own inset management

--- INSPullToRefresh/INSPullToRefreshBackgroundView.m
+++ INSPullToRefresh/INSPullToRefreshBackgroundView.m
@@ -206,19 +206,6 @@ CGFloat const INSPullToRefreshDefaultDragToTriggerOffset = 80;
     }
 }
 
-- (void)didMoveToSuperview {
-    [super didMoveToSuperview];
-    
-    UIViewController *firstReponderViewController = [self ins_firstResponderViewController];
-    firstReponderViewController.automaticallyAdjustsScrollViewInsets = NO;
-    
-    if (firstReponderViewController.navigationController && firstReponderViewController.navigationController.navigationBar.translucent && (firstReponderViewController.edgesForExtendedLayout & UIRectEdgeTop)) {
-        
-        self.scrollView.contentInset = UIEdgeInsetsMake(firstReponderViewController.navigationController.navigationBar.frame.origin.y + firstReponderViewController.navigationController.navigationBar.bounds.size.height, self.scrollView.contentInset.left, self.scrollView.contentInset.bottom, self.scrollView.contentInset.right);
-        self.scrollView.scrollIndicatorInsets = self.scrollView.contentInset;
-    } 
-}
-
 - (void)removeObserversFromView:(UIView *)view {
     NSParameterAssert([view isKindOfClass:[UIScrollView class]]);
 
@@ -243,17 +230,6 @@ CGFloat const INSPullToRefreshDefaultDragToTriggerOffset = 80;
 
 - (void)handleContentInsetDuringFrameChange {
     if (self.shouldResetContentInsetDuringRotation) {
-        UIViewController *viewController = [self ins_firstResponderViewController];
-        
-        BOOL navigationBarHidden = viewController.navigationController.navigationBarHidden;
-        CGFloat navigationBarHeight = viewController.navigationController.navigationBar.frame.origin.y + viewController.navigationController.navigationBar.bounds.size.height;
-        if (navigationBarHidden) {
-            navigationBarHeight = 0.f;
-        }
-        if (viewController.navigationController.navigationBar.translucent && viewController.parentViewController == viewController.navigationController && self.scrollView.frame.origin.y <= navigationBarHeight) {
-            self.externalContentInset = UIEdgeInsetsMake(navigationBarHeight - self.scrollView.frame.origin.y, self.externalContentInset.left, self.externalContentInset.bottom, self.externalContentInset.right);
-        }
-        
         [self resetFrame];
         self.scrollView.scrollIndicatorInsets = self.externalContentInset;
         [self setScrollViewContentInset:self.externalContentInset];
